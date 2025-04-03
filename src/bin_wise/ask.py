e0042a77c62582cdbf9cf5780142774b251c8608
rabbit <rabbit50000t@gmail.com>
2025-04-03 21:47:26 +0900

    第一フェーズ用　仮実装

--- src/bin_wise/ask.py
+++ src/bin_wise/ask.py
@@ -3,7 +3,7 @@ from bin_wise import separate
 from bin_wise import material_checker
 
 def ask(text):
-    target, municipalities = separate.separate(text)  
-    material = material_checker.get_material_info(target)
+    # target, municipalities = separate.separate(text)  
+    # material = material_checker.get_material_info(target)
     #result = VectorStore.search_item(municipalities, material)
-    return result
+    return "燃えるごみです。"
